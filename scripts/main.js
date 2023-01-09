707e2e3a31303ef32940f851d0c24ed18ab12708
KamiRytis <rytiskaminskas0@gmail.com>
2023-01-09 14:01:13 +0200

    event created

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const intro = document.querySelector("#intro");
+intro.addEventListener("click", e =>{
+
+})
+const work = document.querySelector("#work");
+intro.addEventListener("click", e =>{
+    
+})
+const about = document.querySelector("#about");
+intro.addEventListener("click", e =>{
+    
+})
+const contact = document.querySelector("#contact");
+intro.addEventListener("click", e =>{
+    
+})
