d07d9edd3424379c189b8b2d58d60dd895c3378d
KamiRytis <rytiskaminskas0@gmail.com>
2023-01-09 16:15:12 +0200

    funckcionalumas idiegtas

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,44 @@
 const intro = document.querySelector("#intro");
-intro.addEventListener("click", e =>{
-
-})
+const introTab=document.querySelector("#introTab");
 const work = document.querySelector("#work");
-intro.addEventListener("click", e =>{
-    
-})
+const workTab=document.querySelector("#workTab");
 const about = document.querySelector("#about");
-intro.addEventListener("click", e =>{
-    
-})
+const aboutTab=document.querySelector("#aboutTab");
 const contact = document.querySelector("#contact");
+const contactTab=document.querySelector("#contactTab");
+const mainPage = document.querySelector("#mainPage");
+
+
+
 intro.addEventListener("click", e =>{
-    
+    e.preventDefault();
+    mainPage.classList.add("noDisplay");
+    introTab.classList.add("display");
+})
+
+work.addEventListener("click", e =>{
+    e.preventDefault();
+    mainPage.classList.add("noDisplay");
+    workTab.classList.add("display");
+})
+
+about.addEventListener("click", e =>{
+    e.preventDefault();
+    mainPage.classList.add("noDisplay");
+    aboutTab.classList.add("display");
+})
+
+contact.addEventListener("click", e =>{
+    e.preventDefault();
+    mainPage.classList.add("noDisplay");
+    contactTab.classList.add("display");
+})
+document.querySelectorAll("#closeBtn").forEach(button=>{
+    button.addEventListener("click", e =>{
+    e.preventDefault();
+    mainPage.classList.remove("noDisplay");
+    introTab.classList.remove("display");
+    workTab.classList.remove("display");
+    aboutTab.classList.remove("display");
+    contactTab.classList.remove("display");})
 })
